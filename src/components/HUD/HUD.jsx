2436c4689b6c78bd2e494b6588363191486966b3
Viktor Shylumov <shylum033@gmail.com>
2024-04-19 20:03:37 +0300

    add actions from unity to react with arguments

--- src/components/HUD/HUD.jsx
+++ src/components/HUD/HUD.jsx
@@ -2,6 +2,7 @@ import "./HUD.css"
 
 export function HUD(props){
     return <div className="hud">
+        <div className="hud-score">Enemy HP {props.score}</div>
         <button className="hud-attack-button" onClick={props.attackCallback}>Attack</button>
     </div>
 }
--- src/components/UnityContainer/UnityContainer.jsx
+++ src/components/UnityContainer/UnityContainer.jsx
@@ -4,6 +4,7 @@ import {HUD} from "../HUD/HUD";
 import {Onboarding} from "../Onboarding/Onboarding";
 export function UnityContainer() {
   const [isPause, setPause] = useState(true);
+  const [score, setScore] = useState(0);
   const [isShowOnboarding, setOnboarding] = useState(true);
   const {unityProvider, sendMessage, addEventListener, removeEventListener, isLoaded} =
   useUnityContext({
@@ -27,6 +28,32 @@ function handleStartButton(){
   handleResumeButton();
   setOnboarding(false);
 }
+const handleScoreUpdate = useCallback((score)=>{
+  setScore(score);
+});
+
+const handleFloatingText = useCallback((x, y, text)=>{
+  clearTimeout(timeoutID);
+  timeoutID = setTimeout(()=> updateFloatingTexts([]), floatingTextTimer);
+  if(floatingTexts.length > 10){
+      updateFloatingTexts([]);
+  }
+  updateFloatingTexts(arr=>[...arr,<FloatingText x={x} y={y} text={text}/>]);
+});
+
+useEffect(() => {
+  addEventListener("OnScoreUpdate", handleScoreUpdate);
+  return () => {
+      removeEventListener("OnScoreUpdate", handleScoreUpdate);
+  };
+}, [addEventListener, removeEventListener, handleScoreUpdate]);
+
+useEffect(() => {
+  addEventListener("OnFloatingText", handleFloatingText);
+  return () => {
+      removeEventListener("OnFloatingText", handleFloatingText);
+  };
+}, [addEventListener, removeEventListener, handleFloatingText]);
 
 return (<div>
   <Unity unityProvider={unityProvider} style={{width: "100vw", height: "100vh", overflow: "hidden", zIndex: 0}} />
